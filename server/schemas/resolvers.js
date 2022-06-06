b9771e4028abac6746309b94b55cbfb581416577
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-06-06 10:35:00 -0400

    Udated TypeDefs to include Dish model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User, Dish, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
@@ -8,7 +8,7 @@ const resolvers = {
     categories: async () => {
       return await Category.find();
     },
-    products: async (parent, { category, name }) => {
+    dishes: async (parent, { category, name }) => {
       const params = {};
 
       if (category) {
@@ -21,10 +21,10 @@ const resolvers = {
         };
       }
 
-      return await Product.find(params).populate('category');
+      return await Dish.find(params).populate('category');
     },
-    product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
+    dish: async (parent, { _id }) => {
+      return await Dish.findById(_id).populate('category');
     },
     user: async (parent, args, context) => {
       if (context.user) {
@@ -43,7 +43,7 @@ const resolvers = {
     order: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
+          path: 'orders.dishes',
           populate: 'category'
         });
 
