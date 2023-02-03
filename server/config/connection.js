dc4b4fb4ee10dabf3e4bc737eb2e54ab57b393de
Jesse Maun <jesse@Jesses-Air.lan>
2023-02-02 21:42:24 -0500

    reconfigured mongo database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://MichaelMLetanosky:rootroot@cluster0.y0eu9.mongodb.net/atyourdoorset', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Jesse:jesse@cluster0.6fve9.mongodb.net/test', {
+
   useNewUrlParser: true,
   useUnifiedTopology: true,
+
 });
 
 module.exports = mongoose.connection;
